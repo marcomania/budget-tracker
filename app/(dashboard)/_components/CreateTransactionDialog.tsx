62fc8a61ef5e3d7fa903eaf7ec2964a8561e64e3
marcomania <marcow85m@gmail.com>
2024-05-14 23:48:03 -0500

    feat: :sparkles: Dashboard page: create transaction dialog

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -1,15 +1,24 @@
 "use client"
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { TransactionType } from '@/lib/types'
 import { cn } from '@/lib/utils'
 import { CreateTransactionSchema, CreateTransactionSchemaType } from '@/schema/transaction'
-import { ReactNode, useCallback } from 'react'
+import { ReactNode, useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { format } from "date-fns";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import CategoryPicker from './CategoryPicker'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+import { CalendarIcon, Loader2 } from 'lucide-react'
+import { Calendar } from '@/components/ui/calendar'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { CreateTransaction } from '@/lib/actions'
+import { toast } from 'sonner'
+import { DateToUTCDate } from '@/lib/helpers'
 
 interface Props {
   trigger: ReactNode
@@ -17,6 +26,8 @@ interface Props {
 }
 
 const CreateTransactionDialog = ({trigger, type} : Props) => {
+
+  const [open, setOpen] = useState(false);
 
   const form = useForm<CreateTransactionSchemaType>({
     resolver: zodResolver(CreateTransactionSchema),
@@ -29,8 +40,40 @@ const CreateTransactionDialog = ({trigger, type} : Props) => {
     form.setValue("category",value)
   }, [form]);
 
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateTransaction,
+    onSuccess: () => {
+      toast.success("Transaction created succesfully", { id: "create-transaction"})
+
+      form.reset({
+        type,
+        description: "",
+        amount: 0,
+        date: new Date(),
+        category: undefined
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ["overview"],
+      });
+
+      setOpen((prev) => !prev)
+    }
+  });
+
+  const onSubmit = useCallback((values: CreateTransactionSchemaType) => {
+    toast.loading("Creating transaction...", { id: "create-transaction"});
+
+    mutate({
+      ...values,
+      date: DateToUTCDate(values.date)
+    })
+  }, [mutate]);
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         {trigger}
       </DialogTrigger>
@@ -41,7 +84,7 @@ const CreateTransactionDialog = ({trigger, type} : Props) => {
           </DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
             <FormField control={form.control} name='description' render={({field}) => (
               <FormItem>
                 <FormLabel>Description</FormLabel>
@@ -66,7 +109,7 @@ const CreateTransactionDialog = ({trigger, type} : Props) => {
             )}/>
             <div className="flex items-center justify-between gap-3">
               <FormField control={form.control} name='category' render={({field}) => (
-                <FormItem>
+                <FormItem className="flex flex-col">
                   <FormLabel>Category</FormLabel>
                   <FormControl>
                     <CategoryPicker type={type} onChange={handleCategoryChange} />               
@@ -76,9 +119,51 @@ const CreateTransactionDialog = ({trigger, type} : Props) => {
                   </FormDescription>
                 </FormItem>
               )}/>
+
+              <FormField control={form.control} name='date' render={({field}) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Transaction date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button variant={"outline"} className={cn("w-[200px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>
+                          {field.value ? ( format(field.value, "PPP")): ( <span>Pick a date </span>)}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50"></CalendarIcon>
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar 
+                        mode='single'   
+                        selected={field.value} 
+                        onSelect={ (value) => {
+                          if(!value) return;
+                          console.log("@@CALENDAR",value);
+                          field.onChange(value);
+                        }} 
+                        initialFocus 
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormDescription>
+                    Select a date for this transaction
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}/>
             </div>              
           </form>
         </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant={"secondary"} onClick={() => form.reset}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>
+            {isPending ? <Loader2 className="animate-spin"/> : "Create"}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
--- lib/actions.tsx
+++ lib/actions.tsx
@@ -5,8 +5,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
 import { CreateCategorySchema, CreateCategorySchemaType } from "@/schema/categories";
-import { parse } from "path";
-
+import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
 
 export async function UpdateUserCurrency(currency: string) {
   const parsedBody = UpdateUserCurrencySchema.safeParse({ currency })
@@ -35,7 +34,7 @@ export async function UpdateUserCurrency(currency: string) {
 export async function CreateCategory(form: CreateCategorySchemaType) {
   const parsedBody = CreateCategorySchema.safeParse(form)
 
-  if(!parsedBody.success) throw new Error("bad request");
+  if(!parsedBody.success) throw new Error(parsedBody.error.message);
 
   const { userId } = auth();
   if(!userId) {
@@ -49,4 +48,90 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
       userId, name, icon, type,
     }
   });
+}
+
+export async function CreateTransaction(form: CreateTransactionSchemaType) {
+  const parsedBody = CreateTransactionSchema.safeParse(form);
+  if(!parsedBody.success) throw new Error(parsedBody.error.message);
+
+  const { userId } = auth();
+  if(!userId) {
+    redirect("/sign-in")
+  }
+
+  const { amount, category, date, description, type } = parsedBody.data;
+  const categoryRow = await prisma.category.findFirst({
+    where: {
+      userId,
+      name: category
+    }
+  });
+
+  if(!categoryRow) throw new Error("category not found");
+  
+  await prisma.$transaction([
+    prisma.transaction.create({
+      data: { 
+        userId, 
+        amount, 
+        date, 
+        description: description || "",
+        type,
+        category: categoryRow.name,
+        categoryIcon: categoryRow.icon
+      }
+    }),
+
+    prisma.monthHistory.upsert({
+      where: {
+        day_month_year_userId: {
+          userId,
+          day: date.getUTCDay(),
+          month: date.getUTCMonth(),
+          year: date.getFullYear()
+        }
+      },
+      create: {
+        userId,
+        day: date.getUTCDay(),
+        month: date.getUTCMonth(),
+        year: date.getFullYear(),
+        expense: type === "expense" ? amount : 0,
+        income: type === "income" ? amount : 0,
+      },
+      update: {
+        expense: {
+          increment: type === "expense" ? amount : 0, 
+        },
+        income: {
+          increment: type === "income" ? amount : 0, 
+        },
+      }
+    }),
+
+    prisma.yearHistory.upsert({
+      where: {
+        month_year_userId: {
+          userId,
+          month: date.getUTCMonth(),
+          year: date.getFullYear()
+        }
+      },
+      create: {
+        userId,
+        month: date.getUTCMonth(),
+        year: date.getFullYear(),
+        expense: type === "expense" ? amount : 0,
+        income: type === "income" ? amount : 0,
+      },
+      update: {
+        expense: {
+          increment: type === "expense" ? amount : 0, 
+        },
+        income: {
+          increment: type === "income" ? amount : 0, 
+        },
+      }
+    })
+  ])
 }

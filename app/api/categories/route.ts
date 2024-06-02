4943b4f788205ac2fbec4e4f8018a2ee01e68e38
marcomania <marcow85m@gmail.com>
2024-06-02 01:15:20 -0500

    feat: :sparkles: Dashboard page: stats section

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -14,7 +14,7 @@ export async function GET(request:Request) {
   const queryParams = validator.safeParse(paramType)
 
   if(!queryParams.success) {
-    return Response.json(queryParams.error, { status: 400})
+    return Response.json(queryParams.error.message, { status: 400})
   }
   
   const type = queryParams.data;

035728e1b5449bbc969dbc643dee3e9bf35422fb
marcow85m <marcow85m@gmail.com>
2024-06-04 19:36:09 +0000

    fix: :wrench: Adding support for openssl in linux systems

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.openssl
+    pkgs.python311Packages.python-dotenv
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
